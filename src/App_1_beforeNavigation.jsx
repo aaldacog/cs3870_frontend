8984fd9e96be233373f83d4d18468e00c590ec30
Abraham Aldaco <abraham.aldaco@gmail.com>
2025-11-20 21:30:12 -0600

    Initial React/Vite Frontend

---
+++ src/App_1_beforeNavigation.jsx
@@ -0,0 +1,13 @@
+import Contacts from "./Contacts.jsx";
+
+
+function App() {
+
+  return (
+    <>
+      <Contacts />
+    </>
+  )
+}
+
+export default App
---
+++ src/Contacts.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+
+const Contacts = () => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const response = await fetch("https://cs3870-backend-1.onrender.com/contacts");
+        if (!response.ok) {
+          throw new Error("Failed to fetch contacts");
+        }
+        const data = await response.json();
+        setContacts(data);
+      } catch (error) {
+        alert("There was an Error loading contacts " + error.message);
+      }
+    };
+    fetchContacts();
+  }, []);
+
+  return (
+    <div className="container">
+      <ul className="list-group">
+        {contacts.map((contact) => (
+          <li
+            key={contact.id}
+            className="list-group-item d-flex align-items-center"
+          >
+            {contact.image_url && (
+              <img
+                src={contact.image_url}
+                alt={contact.contact_name}
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  marginRight: "15px",
+                  objectFit: "cover",
+                }}
+              />
+            )}
+            <div>
+              <strong>{contact.contact_name}</strong> - {contact.phone_number}
+              <p>{contact.message}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default Contacts;
